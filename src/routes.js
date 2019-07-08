0dda93b28a095754f62d619f1267368564028055
wagner.dutra <wagnerdutra1010@gmail.com>
2019-07-08 18:25:36 -0300

    Adding handler error validation

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 const routes = require('express').Router()
 const validator = require('express-validation')
+const handler = require('express-async-handler')
 
 const ChallengeController = require('./app/controllers/ChallengeController')
 
 const ChallengeValidation = require('../src/app/validators/Challenge')
 
-routes.post('/', validator(ChallengeValidation), ChallengeController.create)
+routes.post(
+  '/',
+  validator(ChallengeValidation),
+  handler(ChallengeController.create)
+)
 
-routes.get('/', ChallengeController.find)
+routes.get('/', handler(ChallengeController.find))
 
 module.exports = routes
